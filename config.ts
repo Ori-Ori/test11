f2a2725f7d90734715d3b6e9c4efd2d6b7e49940
Ori-Ori <126326402+Ori-Ori@users.noreply.github.com>
2025-05-30 09:52:05 -0700

    Update config.ts

--- config.ts
+++ config.ts
@@ -11,8 +11,8 @@ export const window = {
 };
 
 export const api = {
-    ws: 'ws://127.0.0.1:1370/ws',
-    web: 'http://127.0.0.1:1370',
+    ws: 'ws://185.28.84.12:1370/ws',
+    web: 'http://185.28.84.12:1370',
     // extraToken: token,
 };
 
